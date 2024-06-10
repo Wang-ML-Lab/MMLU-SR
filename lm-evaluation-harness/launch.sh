7ed812177fd9c867e0f47ac54d96e458ddb554c6
SkySuperCat <cat@tpsri.org>
2024-06-09 21:21:21 -0400

    upload lmeval and results

---
+++ lm-evaluation-harness/launch.sh
@@ -0,0 +1,1 @@
+python3 eval.py --model hf --model_args "pretrained=meta-llama/Meta-Llama-3-8B,parallelize=True" --tasks mmlusr_question_and_answer  --batch_size 2  --output_path /data/local/cat/results --num_fewshot 5
